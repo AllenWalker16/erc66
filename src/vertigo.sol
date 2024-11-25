a81ae2d7fc5b5e0af003eb16ce9685c7ec911448
Allen <allenwalker1603@gmail.com>
2024-11-25 09:42:18 +0100

    more minor updates

--- src/vertigo.sol
+++ src/vertigo.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 // Import the openzepplin contracts
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-// vertigo is  ERC721 signifies that the contract we are creating imports ERC721 and follows ERC721 contract from openzeppelin
+// vertigo is ERC721 signifies that the contract we are creating imports ERC721 and follows ERC721 contract from openzeppelin
 contract vertigo is ERC721 {
     constructor() ERC721("6ixth Vader", "66ix") {
         // mint an NFT to yourself
